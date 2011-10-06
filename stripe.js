a638cd6f1f7b877e4bd904d1604227cf35424b11
Chris <chris@brackishlake.com>
2011-10-05 22:54:33 -0400

    Initial customer creation, other changes.
    * Support for saving customer card data in the vault.
    * Start implementing a User page for viewing or deleting a customer
    record
    * Checkout form hides card form if a customer record is available
    * Introduced install schema
    * Removal of use of session variables
    * Early implementation of cart system

--- stripe.js
+++ stripe.js
@@ -1,14 +1,15 @@
 (function ($) {
 	Drupal.behaviors.stripeConfig = function(context) {
 		// For now we'll just do the test form.
-		$("#stripe-admin-test").submit(function(event) {
-			console.log('test');
+		$('#stripe-checkout-form').submit(function(event) {
+			
 			// disable the submit button to prevent repeated clicks
 			$('.submit-button').attr("disabled", "disabled");
-
-			var amount = $('.amount').val();
+			
+			var amount = $('input[name=amount]').val();
 			
 			Stripe.createToken({
+				card: $('.card-name').val(),
 				number: $('.card-number').val(),
 				cvc: $('.card-cvc').val(),
 				exp_month: $('.card-expiry-month').val(),
@@ -23,9 +24,12 @@
 	stripeResponseHandler = function(status, response) {
 		console.log(response);
 		if (response.error) {
-			$("#stripe-admin-test").after($('<div class="payment-errors"></div>').html(response.error.message));
+			$("#stripe-checkout-form").after($('<div class="payment-errors"></div>').html(response.error.message));
 		} else {
-			var $form = $("#stripe-admin-test");
+			var $form = $("#stripe-checkout-form");
+				
+   		$('#stripe-checkout-form :input').each(function() {
+    	});
 			
 			// token contains id, last4, and card type
 			var token = response['id'];
